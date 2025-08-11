b17f7e4c73dc613def5e7d807e1089e2dc31ea79
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2025-08-11 10:33:26 +0500

    grafana

---
+++ src/main/java/uz/brb/grafana/controller/HelloController.java
@@ -0,0 +1,17 @@
+package uz.brb.grafana.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("INFO LOG");
+        log.error("ERROR LOG");
+        log.warn("WARN LOG");
+        return "Hello from Spring Boot!";
+    }
+}
